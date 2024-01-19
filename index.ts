4b3628e3eac49eef2f4674a528c677fb73b0155b
Younis <23105954+Younis-Ahmed@users.noreply.github.com>
2024-01-19 15:23:28 +0000

    Add questions and implement game logic

--- index.ts
+++ index.ts
@@ -2,9 +2,10 @@
 
 import * as clack from '@clack/prompts';
 import { setTimeout } from 'node:timers/promises';
+import questions from './questions.json';
 import color from 'picocolors';
 
-let totalQuestions = 0;
+let totalQuestions = 5;
 let correctAnswers = 0;
 
 async function questionDisplay(question: string, mutlipleAnswers: string[], correctAnswerIdx: number) : Promise<void> {
@@ -52,7 +53,35 @@ async function main() {
 
     clack.intro(`${color.bgBlack(color.magenta('Welcome to the Alx trivia!'))}\n\n${color.bgBlack(color.yellow(`You will be asked random Alx questions, You need to answer ${totalQuestions} consecutive to Wins!`))}\n\n${color.bgBlack(color.green('Good luck!'))}`);
 
-    
+    await setTimeout(2000);
 
+    const questionsArray: Question[] = [];
+
+    questions.forEach((question) => {
+        questionsArray.push(new Question(question.question, question.answers, question.correct));
+    });
+
+    const readyToPlay = await clack.select({
+		message: "No cheating. Results at the end. Ready to play?",
+		initialValue: "Yes",
+		options: [
+			{value: "Yes", label: "Yes"},
+			{value: "No", label: "No"}],
+	})
+
+    if (readyToPlay === 'No') {
+        console.log(color.bgBlack(color.red('Bye!')));
+        process.exit(0);
+    }
+
+
+
+    while (totalQuestions) {
+        const randomQuestion = Math.floor(Math.random() * questionsArray.length);
+        const question = questionsArray[randomQuestion];
+        await questionDisplay(question.question, question.answersArray, question.correctAnswerIndex);
+        totalQuestions--;
+    }
 
 }
+main().catch(console.error);
