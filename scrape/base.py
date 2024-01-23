e7617343d8587f820a5f9b5be8f055df77c038bb
jonaahmed <23105954+jonaahmed@users.noreply.github.com>
2024-01-23 17:16:58 +0300

    Add environment variables for email and password

--- scrape/base.py
+++ scrape/base.py
@@ -1,4 +1,8 @@
 from playwright.sync_api import Playwright
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 def run(playwright: Playwright) -> None:
@@ -6,8 +10,8 @@ def run(playwright: Playwright) -> None:
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://intranet.alxswe.com/auth/sign_in")
-    page.get_by_label("Email").fill()
+    page.get_by_label("Email").fill(os.getenv("EMAIL"))
     page.get_by_label("Password").click()
-    page.get_by_label("Password").fill("")
+    page.get_by_label("Password").fill(os.getenv("PASSWORD"))
     page.get_by_label("Remember me").check()
     page.get_by_role("button", name="Log in").click()
