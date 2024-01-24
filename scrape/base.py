b7e00dae6a0a370772bf8d95a8773ba69d1465aa
jonaahmed <23105954+jonaahmed@users.noreply.github.com>
2024-01-24 10:51:45 +0300

    Add quiz functionality to login process

--- scrape/base.py
+++ scrape/base.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Playwright
+from playwright.sync_api import Playwright, sync_playwright, expect
 from dotenv import load_dotenv
 import os
 
@@ -15,3 +15,15 @@ def run(playwright: Playwright) -> None:
     page.get_by_label("Password").fill(os.getenv("PASSWORD"))
     page.get_by_label("Remember me").check()
     page.get_by_role("button", name="Log in").click()
+    page.goto("https://intranet.alxswe.com/projects/100021")
+    page.mouse.wheel(0, 1000)
+    page.get_by_text("(Show quiz)").click()
+    html = page.inner_html(".quiz_questions_show_container")
+    print(html)
+    context.close()
+    browser.close()
+
+
+if __name__ == "__main__":
+    with sync_playwright() as playwright:
+        run(playwright)
