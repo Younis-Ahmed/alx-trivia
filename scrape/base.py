dfca2f270d3e50f7717268d1a1f2d3032c79021b
jonaahmed <23105954+jonaahmed@users.noreply.github.com>
2024-01-24 11:09:28 +0300

    Add BeautifulSoup for HTML parsing and extract quiz questions and answers

--- scrape/base.py
+++ scrape/base.py
@@ -1,6 +1,8 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
 from dotenv import load_dotenv
 import os
+from bs4 import BeautifulSoup
+
 
 load_dotenv()
 
@@ -19,7 +21,15 @@ def run(playwright: Playwright) -> None:
     page.mouse.wheel(0, 1000)
     page.get_by_text("(Show quiz)").click()
     html = page.inner_html(".quiz_questions_show_container")
-    print(html)
+    # print(html)
+    soup = BeautifulSoup(html, "html.parser")
+    rel = soup.find_all("div", class_="clearfix")
+    for i in rel:
+        print(i.find("p").text)
+        for j in i.find_all("li"):
+            print(j.text)
+        print("-----")
+
     context.close()
     browser.close()
 
